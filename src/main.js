4f1d2b8231992f69905c00cb38c53fcf769270ee
BehnamBeiki <behnam_beiki@yahoo.com>
2025-02-14 23:37:41 +0330

    update

--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
 import Phaser from 'phaser';
 import { CONFIG } from './config';
 import { SCENES } from './scenes';
-import Resize from './resize';
-// import { Resize } from './resize';
+// import Resize from './resize';
 
-const game = new Phaser.Game({
+export default new Phaser.Game({
   ...CONFIG,
   scene: SCENES,
 });
 
-game.events.once('Boot', () => {
-  const resizeHandler = new Resize(game);
-  resizeHandler.init();
-});
+// const game = new Phaser.Game({
+//   ...CONFIG,
+//   scene: SCENES,
+// });
 
-// window.addEventListener('resize', () => Resize(game));
-// game.events.on('ready', () => {
-//   Resize(game);
+// game.events.once('Boot', () => {
+//   const resizeHandler = new Resize(game);
+//   resizeHandler.init();
 // });
--- src/resize.js
+++ src/resize.js
@@ -13,27 +13,3 @@ export default class Resize {
     );
   }
 }
-
-// export function Resize(game) {
-//   const targetAspectRatio = 1920 / 1080; // Desired aspect ratio (4:3)
-//   const windowWidth = window.innerWidth;
-//   const windowHeight = window.innerHeight;
-//   const windowAspectRatio = windowWidth / windowHeight;
-
-//   let newWidth, newHeight;
-
-//   if (windowAspectRatio > targetAspectRatio) {
-//     // Window is too wide, limit height
-//     newHeight = windowHeight;
-//     newWidth = newHeight * targetAspectRatio;
-//   } else {
-//     // Window is too tall, limit width
-//     newWidth = windowWidth;
-//     newHeight = newWidth / targetAspectRatio;
-//   }
-
-//   // Resize the Phaser game
-//   game.scale.resize(newWidth, newHeight);
-//   game.canvas.style.width = `${newWidth}px`;
-//   game.canvas.style.height = `${newHeight}px`;
-// }

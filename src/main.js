51e891792fffc1b317e5ad0c3106ef12da4df994
BehnamBeiki <behnam_beiki@yahoo.com>
2025-02-14 15:55:10 +0330

    update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
-import config from './config';
+import { Boot } from './scenes/Boot';
+import { Preloader } from './scenes/Preloader';
+import { MainMenu } from './scenes/MainMenu';
+import { Game } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1024,
+  height: 768,
+  parent: 'game-container',
+  // backgroundColor: "#028af8",
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: [Boot, Preloader, MainMenu, Game, GameOver],
+};
 
 export default new Phaser.Game(config);

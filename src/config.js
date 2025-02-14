5f99892615c72a99224f7ba091b7279169c2296c
BehnamBeiki <behnam_beiki@yahoo.com>
2025-02-15 00:09:56 +0330

    update

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export const CONFIG = {
   parent: 'game-container',
   // backgroundColor: "#028af8",
   scale: {
-    mode: Phaser.Scale.RESIZE,
+    mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
 };
--- src/resizeWindow.js
+++ src/resizeWindow.js
@@ -3,18 +3,3 @@ export function resizeWindow(game) {
     game.scale.resize(window.innerWidth, window.innerHeight);
   });
 }
-
-// export default class Resize {
-//   constructor(game) {
-//     this.game = game;
-//   }
-//   init() {
-//     this.game.scale.on('resize', this.resize.bind(this));
-//     this.resize(
-//       this.game.scale.gameSize,
-//       this.game.scale.baseSize,
-//       this.game.scale.displaySize,
-//       this.game.scale.resolution
-//     );
-//   }
-// }
